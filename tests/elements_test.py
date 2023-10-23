e6c0090cb24e455ccc0719141eefc12e0a47bc89
Artyom Ceburaskin <a.ceburaskins@gmail.com>
2023-10-23 16:06:35 +0300

    Added check boxes tests

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,6 +1,7 @@
 import time
 
-from pages.elements_page import TextBoxPage
+from pages.elements_page import TextBoxPage, CheckBoxPage
+from conftest import driver
 
 
 class TestElements:
@@ -12,8 +13,21 @@ class TestElements:
             full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
             output_name, output_email, output_cur_addr, output_perm_addr = text_box_page.check_filled_form()
 
-            assert full_name == output_name
-            assert email == output_email
-            assert current_address == output_cur_addr
-            assert permanent_address == output_perm_addr
-            time.sleep(5)
+            assert full_name == output_name, "Full name doesn't match"
+            assert email == output_email, "Email doesn't match"
+            assert current_address == output_cur_addr, "Current address doesn't match"
+            assert permanent_address == output_perm_addr, "Permanent address doesn't match"
+
+    class TestCheckBox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, 'https://demoqa.com/checkbox')
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            input_checkbox = check_box_page.get_checked_checkboxes()
+            output_result = check_box_page.get_output_result()
+            print(input_checkbox)
+            print(output_result)
+            assert input_checkbox == output_result, "Checkboxes titles don't match with output result"
+
+
